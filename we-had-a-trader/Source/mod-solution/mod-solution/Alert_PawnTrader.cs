fb5cbe62abf740b77a13bac84cc57e6c8756f074
liebasam <l.samuel.cook@gmail.com>
2017-02-16 16:15:50 -0500

    init commit we-had-a-trader

---
+++ we-had-a-trader/Source/mod-solution/mod-solution/Alert_PawnTrader.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+
+namespace RimWorld
+{
+    public class Alert_PawnTrader : Alert
+    {
+        
+        private IEnumerable<Pawn> TraderPawns
+        {
+            get
+            {
+                return PawnsFinder.AllMaps_Spawned.Where<Pawn>((Func<Pawn,bool>)(p =>
+                {
+                    return (p.trader != null && p.trader.CanTradeNow);
+                }));
+            }
+        }
+        
+        public override string GetLabel()
+        {
+            if (this.TraderPawns.Count<Pawn>() > 1)
+                return string.Format("PawnTrader".Translate(), "s");
+            return string.Format("PawnTrader".Translate(), "");
+        }
+        public override string GetExplanation()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            foreach(Pawn pawn in this.TraderPawns)
+                stringBuilder.AppendLine("    " + pawn.Faction.Name);
+
+            return string.Format("PawnTraderDesc".Translate(), stringBuilder.ToString());
+        }
+
+        public override AlertReport GetReport()
+        {
+            if (this.TraderPawns.Count<Pawn>() > 0)
+                return (AlertReport)true;
+            return (AlertReport)false;
+        }
+
+        
+    }
+}
